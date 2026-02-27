31d5478f823a89d7a03ea50f9957176ce7b71b35
Ziad Nader <ziad.nader88@gmail.com>
2026-02-27 19:23:03 +0100

    vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,15 @@ export default defineConfig(({ mode }) => ({
       // Local dev helper to avoid CORS when hitting FastAPI directly.
       // Use `VITE_FPL_TEAM_RECOMMENDATION_URL="/recommendations?..."`
       "/recommendations": {
-        target: "http://127.0.0.1:8001",
+        target: "https://fpl-refresh-app.whitestone-9a91372e.francecentral.azurecontainerapps.io/",
         changeOrigin: true,
       },
       "/squad": {
-        target: "http://127.0.0.1:8001",
+        target: "https://fpl-refresh-app.whitestone-9a91372e.francecentral.azurecontainerapps.io/",
         changeOrigin: true,
       },
       "/fixtures": {
-        target: "http://127.0.0.1:8001",
+        target: "https://fpl-refresh-app.whitestone-9a91372e.francecentral.azurecontainerapps.io/",
         changeOrigin: true,
       },
     },
